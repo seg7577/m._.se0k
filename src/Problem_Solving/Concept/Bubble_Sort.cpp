123e1300755dd3b2cbd4fbb50c5653f32307c9fa
m._.se0k_space <seg082911@gmail.com>
2024-07-13 20:11:57 +0900

    20240712

--- src/Problem_Solving/Concept/Bubble_Sort.cpp
+++ src/Problem_Solving/Concept/Bubble_Sort.cpp
@@ -1,6 +1,7 @@
 void BubbleSort(int DataSet[], int Length)
 {
     //DataSet을 순회하며 이웃한 요소들끼리 값을 교환하며 정렬을 수행한다.
+    //시간 복잡도 O(n^2)
     int i = 0, j = 0, temp = 0;
     for (i = 0; i < Length - 1; i++)
     {

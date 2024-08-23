b5701ca35d42282610815cda413be6daae392857
m._.se0k_space <seg082911@gmail.com>
2024-08-23 23:22:48 +0900

    update

---
+++ Swift_ grammar/Swift_ grammar/practice01.swift
@@ -0,0 +1,39 @@
+//
+//  practice01.swift
+//  Swift_ grammar
+//
+//  Created by m._.se0k on 8/23/24.
+//
+
+import SwiftUI
+
+struct practice01: View {
+    @State var menu: String? = "오늘 뭐 먹지"
+    let menus:[Menu] = [.apple, .banana, .sushi, .pho]
+    
+    enum Menu: String{
+        case apple = "사과"
+        case banana = "바나나"
+        case sushi = "스시"
+        case pho = "쌀국수"
+    }
+    
+    var body: some View {
+        VStack{
+            if menu == "사과" || menu == "스시"{
+                Text("너무 맛있는 \(menu!)")
+            } else{
+                Text(menu!)
+            }
+            Button{
+                menu = menus.randomElement()?.rawValue
+            }label:{
+                Text("눌러요!")
+            }
+        }
+    }
+}
+
+#Preview {
+    practice01()
+}

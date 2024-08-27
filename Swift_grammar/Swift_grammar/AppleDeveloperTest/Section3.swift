dd07cd1ac895ccb98eb9ebb4731bd12cc4ec875a
m._.se0k_space <seg082911@gmail.com>
2024-08-28 02:38:43 +0900

    20240828

---
+++ Swift_grammar/Swift_grammar/AppleDeveloperTest/Section3.swift
@@ -0,0 +1,18 @@
+//
+//  Section3.swift
+//  Swift_grammar
+//
+//  Created by m._.se0k on 8/27/24.
+//
+
+import SwiftUI
+
+struct Section3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Section3()
+}
---
+++ Swift_grammar/Swift_grammar/MtStruct.swift
@@ -0,0 +1,18 @@
+//
+//  MtStruct.swift
+//  Swift_grammar
+//
+//  Created by m._.se0k on 8/27/24.
+//
+
+import SwiftUI
+
+struct MtStruct: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MtStruct()
+}

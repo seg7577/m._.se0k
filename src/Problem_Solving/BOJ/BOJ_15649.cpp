bc334f69eb4f14529fca8662844ee3c0246d2ca4
m._.se0k_space <seg082911@gmail.com>
2024-09-07 02:20:41 +0900

    20240907

--- src/Problem_Solving/BOJ/BOJ_15649.cpp
+++ src/Problem_Solving/BOJ/BOJ_15649.cpp
@@ -1,44 +1,90 @@
+// // #include <iostream>
+// // #include <vector>
+
+// // using namespace std;
+
+// // bool vis[8];
+// // int n, m;
+// // int arry[8];
+// // vector<int> vec;
+// // void dfs(int cnt);
+// // int main()
+// // {
+// // 	ios_base::sync_with_stdio(false);
+// // 	cin.tie(0);
+// // 	cout.tie(0);
+
+// // 	cin >> n >> m;
+	
+// // 	for (int i = 0; i < n; i++)
+// // 		arry[i] = i + 1;
+
+// // 	dfs(0);
+// // 	return 0;
+// // }
+// // void dfs(int cnt)
+// // {
+// // 	if (cnt == m)
+// // 	{
+// // 		for (int i = 0; i < m; i++)
+// // 			cout << vec[i] << ' ';
+// // 		cout << '\n';
+// // 		return;
+// // 	}
+// // 	for (int i = 0; i < n; i++)
+// // 	{
+// // 		if (vis[i])	continue;
+// // 		vis[i] = true;
+// // 		vec.push_back(arry[i]);
+// // 		dfs(cnt + 1);
+// // 		vec.pop_back();
+// // 		vis[i] = false;
+// // 	}
+// // 	return;
+// // }
 // #include <iostream>
-// #include <vector>
 
 // using namespace std;
 
-// bool vis[8];
 // int n, m;
-// int arry[8];
-// vector<int> vec;
-// void dfs(int cnt);
+// bool vis[10];
+// int arr[10];
+
+// void func(int k)
+// {
+//     if (k == m)
+//     //배열이 m번째의 인덱스까지 모두 채워진 경우에 대한 처리 (즉, 길이가 m인 수열이 완성된 경우)
+//     {
+//         for (int i = 0; i < m; i++)
+//         //완성된 m개의 수열을 출력
+//             cout << arr[i] << ' ';
+//         cout << '\n';
+//         return;
+//     }
+    
+//     for (int i = 1; i <= n; i++)
+//     //1부터 n까지의 수를 차례로 시도
+//     {
+//         if (!vis[i])
+//         //아직 해당 숫자[i]가 수열에 사용되지 않았다면
+//         {
+//             arr[k] = i;
+//             vis[i] = true;
+//             //i를 현재 위치 k에 넣고, 사용했음을 표시
+//             func(k+1);
+//             vis[i] = false;
+//             //재귀 호출 이후 다시 해당 숫자를 사용하지 않았다고 표시
+//         }
+//     }
+// }
+
+
 // int main()
 // {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
-
-// 	cin >> n >> m;
-	
-// 	for (int i = 0; i < n; i++)
-// 		arry[i] = i + 1;
-
-// 	dfs(0);
-// 	return 0;
-// }
-// void dfs(int cnt)
-// {
-// 	if (cnt == m)
-// 	{
-// 		for (int i = 0; i < m; i++)
-// 			cout << vec[i] << ' ';
-// 		cout << '\n';
-// 		return;
-// 	}
-// 	for (int i = 0; i < n; i++)
-// 	{
-// 		if (vis[i])	continue;
-// 		vis[i] = true;
-// 		vec.push_back(arry[i]);
-// 		dfs(cnt + 1);
-// 		vec.pop_back();
-// 		vis[i] = false;
-// 	}
-// 	return;
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+    
+//     cin >> n >> m;
+//     func(0);
 // }
--- src/Problem_Solving/BOJ/BOJ_15651.cpp
+++ src/Problem_Solving/BOJ/BOJ_15651.cpp
@@ -1,44 +1,93 @@
-// #include <iostream>
-// #include <vector>
+#include <iostream>
+#include <vector>
 
-// using namespace std;
+using namespace std;
 
-// int n, m;a
-// bool vis[7];
-// int arry[7];
-// bool duplication[7];
-// vector<int>vec;
+int n, m;a
+bool vis[7];
+int arry[7];
+bool duplication[7];
+vector<int>vec;
 
-// void dfs(int cnt);
+void dfs(int cnt);
 
-// int main()
-// {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-// 	cin >> n >> m;
-// 	for (int i = 0; i < n; i++)
-// 		arry[i] = i + 1;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		arry[i] = i + 1;
 
-// 	dfs(0);
-// }
-// int Count = 1;
-// void dfs(int cnt)
-// {
-// 	if (cnt == m)
-// 	{
-// 		for (int i = 0; i < m; i++)
-// 		{
-// 			cout << vec[i] << ' ';
-// 		}
-// 		cout << '\n';
-// 		return;
-// 	}
-// 	for (int i = 0; i < n; i++)
-// 	{
-// 		vec.push_back(arry[i]);
-// 		dfs(cnt + 1);
-// 		vec.pop_back();
-// 	}
-// }
+	dfs(0);
+}
+int Count = 1;
+void dfs(int cnt)
+{
+	if (cnt == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << vec[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		vec.push_back(arry[i]);
+		dfs(cnt + 1);
+		vec.pop_back();
+	}
+}
+/*
+문제 : 자연수 n과 m이 주어졌을 때, 아래 조건을 만족하는 길이가 m인 수열을 모두 구하는 프로그램을 작성하시오.
+ 1. 1부터 n까지 자연수 중에서 m개를 고른 수열
+ 2. 같은 수를 여러 번 골라도 된다.
+ -------------------------------------------------------------------------------------------------------------------
+입력 : 첫째 줄에 자연수 n과 m이 주어진다. (1 <= M <= N <= 7)
+출력 : 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열을 공배긍로 구분해서 출력해야 한다.
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+ -------------------------------------------------------------------------------------------------------------------
+풀이 : 앞서 풀었던 n과m(1), (2)의 문제의 조건에 해당 됐던 중복된 수는 수열에서 제외된다. 조건을 처리하기 위해 사용됐던 visted 배열을 사용하지 않으면 쉽게 풀이가 가능했다.
+*/
+
+#include <iostream>
+#define MAX 10
+
+using namespace std;
+
+int n, m;
+int arr[MAX];
+
+void func(int k)
+{
+    if (k == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        
+        return;
+    }
+    
+    for (int i = 1; i <= n; i++)
+    {
+        arr[k] = i ;
+        func(k + 1);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> n >> m;
+    func(0);
+    
+    return 0;
+}
